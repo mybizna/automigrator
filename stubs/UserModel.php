f31c2534f05debf49fca19c0e6addd28ec4e1820
legodion <legodion@icloud.com>
2022-03-25 12:32:26 -0400

    user stub tweak

--- stubs/UserModel.php
+++ stubs/UserModel.php
@@ -30,7 +30,7 @@ class DummyClass extends Authenticatable
     public function definition(Generator $faker)
     {
         return [
-            'name' => $faker->name(),
+            'name' => $faker->firstName(),
             'email' => $faker->unique()->safeEmail(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'created_at' => $faker->dateTimeThisMonth(),
